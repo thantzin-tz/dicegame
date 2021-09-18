1ffb6dc278b00573ad122f2b623a6af5acce13c9
Thant Zin Lin <76914297+thantzin-tz@users.noreply.github.com>
2021-09-18 09:51:52 +0630

    Add JavaScript file

---
+++ javascript/index.js
@@ -0,0 +1,43 @@
+let player1 = prompt("Enter Player-1's Name ")
+let player2 = prompt("Enter Player-2's  Name ")
+
+if(player1 === null){
+    document.querySelector("#player1").textContent = "Player 1";
+}else{
+    document.querySelector("#player1").textContent = player1
+}
+if(player2 === null){
+    document.querySelector("#player2").textContent = "Player 2"
+}else{
+    document.querySelector("#player2").textContent = player2
+}
+
+function play(p1, p2){
+    let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+    let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+   
+
+    document.querySelector(".img1").setAttribute("src","images/dice"+randomNumber1+".png");
+    document.querySelector(".img2").setAttribute("src","images/dice"+randomNumber2+".png");
+ 
+    if(randomNumber1 > randomNumber2){
+       if(player1 === null){
+        document.querySelector("h1").textContent = `Player 1 🏆 Wins! 🎉`;
+       }else{
+        document.querySelector("h1").textContent = `${player1} 🏆 Wins! 🎉`;
+       }
+    } else if(randomNumber1 === randomNumber2){
+        document.querySelector("h1").textContent = "Draw! 🤝";
+    }else{
+        if(player2 === null){
+            document.querySelector("h1").textContent = `Player 2 🏆 Wins! 🎉`;
+        }else{
+            document.querySelector("h1").textContent = `${player2} 🏆 Wins! 🎉`;
+        }
+    }
+ 
+}
+
+document.querySelector("#btn").setAttribute("onclick", "play(player1, player2)");
